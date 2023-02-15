92fde924aa55d4b8b8656144cf441f1e57cd041e
Nicolas Cervino <nicolascervino2@gmail.com>
2023-02-14 21:58:18 -0300

    add workout banner component (placeholder data)

--- pages/app.tsx
+++ pages/app.tsx
@@ -2,12 +2,16 @@ import Header from "@/components/Header";
 import React from "react";
 import withAuth from "@/hocs/withAuth";
 import SearchBar from "@/components/SearchBar";
+import Footer from "@/components/Footer";
+import LatestWorkoutBanner from "@/components/LatestWorkoutBanner";
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-[#151515]">
       <Header />
       <SearchBar />
+      <LatestWorkoutBanner />
+      <Footer />
     </div>
   );
 };
