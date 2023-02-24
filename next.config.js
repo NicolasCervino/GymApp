8afaf7a888cc6f7a6ba4e98177cde54eefe0dfc8
Nicolas Cervino <nicolascervino2@gmail.com>
2023-02-23 23:15:11 -0300

    add exercise gif domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com", "d205bpvrqc9yn1.cloudfront.net"],
   },
 };
 
