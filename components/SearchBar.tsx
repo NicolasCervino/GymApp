03eef8786678c26940351650e58ca1892fd16ace
Nicolas Cervino <nicolascervino2@gmail.com>
2023-02-24 22:22:22 -0300

    searchbar uses onSearch function that comes by props

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
-function SearchBar() {
+function SearchBar({ onSearch }: { onSearch: (query: string) => void }) {
   const [query, setQuery] = useState<string>("");
 
-  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" && query !== "") alert(query);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearch(query);
   };
 
   return (
-    <div className="w-full p-6 outline-none flex items-center">
+    <form className="w-full p-6 outline-none flex items-center" onSubmit={handleSubmit}>
       <label className="bg-[#252525] h-8 flex items-center px-6 rounded-l-xl">
         <BsSearch />
       </label>
       <input
         className="outline-none w-full bg-[#252525] h-8 rounded-r-xl"
         type="text"
-        placeholder="Search exercises"
-        onKeyDown={handleSearch}
+        placeholder="Search exercises by name"
         onChange={(e) => setQuery(e.target.value)}
       />
-    </div>
+    </form>
   );
 }
 
