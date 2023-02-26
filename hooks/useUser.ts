408e0798d8553f99d2cdc821ad5016d43de4efb8
Nicolas Cervino <nicolascervino2@gmail.com>
2023-02-26 17:55:52 -0300

    updated imports

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,6 +1,6 @@
 import { UserData } from "@/interfaces/userData";
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/auth/AuthContext";
 
 export const useUser = (): UserData | null => {
   const userContext = useContext(AuthContext);
