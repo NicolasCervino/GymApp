7b7ffbec0881af92012dfbc305fc5e000fa3b683
Nicolas Cervino <nicolascervino2@gmail.com>
2023-03-07 00:53:52 -0300

    save button working

--- components/RoutineHeader.tsx
+++ components/RoutineHeader.tsx
@@ -1,3 +1,5 @@
+import { useRoutineContext } from "@/context/routine/RoutineProvider";
+import { useSweetAlert } from "@/utils/useSwal";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { AiOutlineLeft, AiOutlineSave } from "react-icons/ai";
@@ -5,18 +7,41 @@ import { AiOutlineLeft, AiOutlineSave } from "react-icons/ai";
 const RoutineHeader = () => {
   const [addExerciseMode, setAddExerciseMode] = useState<boolean>(false);
   const router = useRouter();
+  const { newRoutine, setNewRoutine, saveNewRoutine } = useRoutineContext();
+  const { showAlert, showingAlert } = useSweetAlert();
 
   useEffect(() => {
     setAddExerciseMode(router.asPath.includes("/add-exercise"));
   }, [router.asPath]);
 
-  const handleSaveRoutine = () => {
-    console.log("test");
+  const handleSaveRoutine = async () => {
+    if (newRoutine && newRoutine.name && newRoutine.tasks.length > 0) {
+      const success = await saveNewRoutine();
+      if (success) {
+        showAlert("Routine saved", "success", () => {
+          setNewRoutine(null);
+          router.back();
+        });
+      } else {
+        showAlert("Failed to save routine", "error");
+      }
+    } else {
+      showAlert("Routine must have at least 1 task and a name", "error");
+    }
+  };
+
+  const handleBackButton = () => {
+    if (router.asPath.includes("/add-exercise")) {
+      router.back();
+    } else {
+      setNewRoutine(null);
+      router.back();
+    }
   };
 
   return (
     <div className="bg-[#151515] min-h-[8vh] flex items-center justify-between px-4 text-white">
-      <button className="border-white border-2 p-2 rounded-lg hover:bg-slate-500" onClick={router.back}>
+      <button disabled={showingAlert} className="border-white border-2 p-2 rounded-lg hover:bg-slate-500" onClick={handleBackButton}>
         <AiOutlineLeft className="w-5 h-5" />
       </button>
       <div className="flex-1 flex items-center justify-center">
@@ -24,7 +49,7 @@ const RoutineHeader = () => {
       </div>
       {/* Save button */}
       {!addExerciseMode && (
-        <button className="border-white border-2 p-2 rounded-lg hover:bg-slate-500" onClick={handleSaveRoutine}>
+        <button disabled={showingAlert} className="border-white border-2 p-2 rounded-lg hover:bg-slate-500" onClick={handleSaveRoutine}>
           <AiOutlineSave className="w-5 h-5" />
         </button>
       )}
