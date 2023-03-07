5e89c50b8a549b2f0dbe745f084614a60b325924
Nicolas Cervino <99894603+NicolasCervino@users.noreply.github.com>
2023-03-07 02:01:32 -0300

    RoutineBanner shows the actual routine info

--- components/RoutineBanner.tsx
+++ components/RoutineBanner.tsx
@@ -1,10 +1,24 @@
+import { Routine } from "@/interfaces/routine";
 import React from "react";
+import { FiTrash2 } from "react-icons/fi";
 
-function RoutineBanner() {
+function RoutineBanner({ routine }: { routine: Routine }) {
+  const handleDeleteRoutine = () => {};
   return (
-    <div className="bg-[#252525] p-3">
-      <h3 className="font-bold text-xl">Routine name</h3>
-      <p className="text-base text-gray-400">Benchpress, Ball slams, Squat (Barbell), Deadlift (Barbell)</p>
+    <div className="bg-[#252525] p-3 flex flex-col justify-between">
+      <div className="flex mb-2">
+        <h3 className="font-bold text-xl">{routine.name}</h3>
+        <button className="ml-auto" onClick={handleDeleteRoutine}>
+          <FiTrash2 size={18} className="hover:text-red-700" />
+        </button>
+      </div>
+      <p className="text-base text-gray-400 capitalize mb-2">
+        {routine.tasks
+          .slice(0, 3)
+          .map((task) => task.exercise.name)
+          .join(", ")}
+        {routine.tasks.length > 3 && "..."}
+      </p>
       <button className="w-full p-2 my-2 bg-[#25ab75] hover:bg-[#1f8b60] text-lg font-semibold rounded-lg ">Start Routine</button>
     </div>
   );
