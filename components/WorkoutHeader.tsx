7de77d94796012aff1dae5dbe63a0674305552fa
Nicolas Cervino <99894603+NicolasCervino@users.noreply.github.com>
2023-03-08 00:30:35 -0300

    add WorkoutHeader

---
+++ components/WorkoutHeader.tsx
@@ -0,0 +1,38 @@
+import useRoutines from "@/hooks/useRoutines";
+import { Routine } from "@/interfaces/routine";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { AiOutlineLeft } from "react-icons/ai";
+import { BsCheck2 } from "react-icons/bs";
+
+export const WorkoutHeader = () => {
+  const [routine, setRoutine] = useState<Routine | null>(null);
+  const router = useRouter();
+  const { routineId } = router.query;
+  const { routines } = useRoutines();
+
+  useEffect(() => {
+    const routine = routines.find((r) => r.id === routineId);
+    setRoutine(routine ?? null);
+  }, [routines, routineId]);
+
+  const handleBackButton = () => {
+    router.back();
+  };
+  const handleFinishWorkout = () => {};
+
+  return (
+    <div className="bg-[#151515] min-h-[8vh] flex items-center justify-between px-4 text-white">
+      <button className="border-white border-2 p-2 rounded-lg hover:bg-slate-500" onClick={handleBackButton}>
+        <AiOutlineLeft className="w-5 h-5" />
+      </button>
+      <div className="flex-1 flex items-center justify-center">
+        <h1 className="text-xl font-bold">{routine?.name}</h1>
+      </div>
+      {/* Finish button */}
+      <button className="border-white border-2 p-2 rounded-lg hover:bg-slate-500" onClick={handleFinishWorkout}>
+        <BsCheck2 className="w-5 h-5" />
+      </button>
+    </div>
+  );
+};
--- layout/appLayout.tsx
+++ layout/appLayout.tsx
@@ -1,20 +1,35 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import RoutineHeader from "@/components/RoutineHeader";
+import { WorkoutHeader } from "@/components/WorkoutHeader";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   const [routineMode, setRoutineMode] = useState<boolean>(false);
+  const [workoutMode, setWorkoutMode] = useState<boolean>(false);
   const router = useRouter();
 
   useEffect(() => {
     setRoutineMode(router.asPath.includes("/new-routine"));
+    setWorkoutMode(router.asPath.includes("/workout/routine"));
   }, [router.asPath]);
+
+  const renderHeader = () => {
+    if (routineMode) {
+      return <RoutineHeader />;
+    }
+
+    if (workoutMode) {
+      return <WorkoutHeader />;
+    }
+
+    return <Header />;
+  };
 
   return (
     <div className="min-h-screen bg-[#151515] text-white">
-      {routineMode ? <RoutineHeader /> : <Header />}
+      {renderHeader()}
       {children}
       {routineMode ? "" : <Footer />}
     </div>
