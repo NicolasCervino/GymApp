3a775c061e465ee070cc3b343c27774d1f7885db
Nicolas Cervino <99894603+NicolasCervino@users.noreply.github.com>
2023-03-18 15:38:11 -0300

    added supabase domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["lh3.googleusercontent.com", "d205bpvrqc9yn1.cloudfront.net"],
+    domains: ["lh3.googleusercontent.com", "d205bpvrqc9yn1.cloudfront.net", "dpkgyzzgvsfjlqyyhmej.supabase.co"],
   },
 };
 
