7a9ce5faa3743b98b9c725fa68a2fe5b7b3f35db
Nicolas Cervino <99894603+NicolasCervino@users.noreply.github.com>
2023-03-25 21:34:57 -0300

    minor changes

--- components/AllWorkouts.tsx
+++ components/AllWorkouts.tsx
@@ -4,7 +4,7 @@ import WorkoutBanner from "./WorkoutBanner";
 
 const AllWorkouts = ({ workouts }: { workouts: Workout[] }) => {
   return (
-    <div className="flex flex-col items-center md:items-start gap-4 m-5 text-white">
+    <div className="flex flex-col items-center md:items-start gap-4 m-5 text-white mb-20">
       <h1 className="font-bold text-2xl self-start">All Workouts</h1>
       {workouts.length === 0 ? (
         <p>You haven’t worked out yet...</p>
