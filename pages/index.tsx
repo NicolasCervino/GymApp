f2c7615d7f8cb93ec2224c2e0a0eeea0c2f123bc
Nicolas Cervino <99894603+NicolasCervino@users.noreply.github.com>
2023-03-28 15:51:06 -0300

    added app name on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import publicOnly from "@/hocs/publicOnly";
 import IndexLayout from "@/layout";
 import Link from "next/link";
+import { BiDumbbell } from "react-icons/bi";
 
 const LandingPage = () => {
   return (
-    <IndexLayout extraClass="items-center text-white">
+    <IndexLayout extraClass="text-white">
+      <div className="flex items-center gap-2 mt-20 w-fit h-fit text-center select-none">
+        <BiDumbbell className="text-4xl text-primary-green" />
+        <h1 className="text-3xl font-bold">Ego-Lifting</h1>
+      </div>
       <div className="w-full md:w-fit mx-auto px-6 sm:h-fit gap-5 bg-white absolute bottom-0 rounded-t-3xl flex flex-col items-center justify-around">
         <h2 className="mt-9 mx-4 text-black text-3xl text-center font-bold">Get a better life and have a healthy body</h2>
         <p className="text-gray-500 font-medium w-[60%] text-center">Spend your fun time at home practicing anytime anywhere</p>
